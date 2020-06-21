4379bf322a7ca2cd7170e782891c4d8aa5d76af3
luferrari <lucialuferrari@gmail.com>
2020-06-22 00:46:06 +0200

    Fifth pass

---
+++ docs/js/lang.js
@@ -0,0 +1,43 @@
+var it = function(){
+	$("[name='__edu-header'").html('<i class="fas fa-sm fa-graduation-cap"></i> Istruzione');
+	$("[name='__edu-01-h3']").html("<strong>Laurea Magistrale</strong> in <strong>Design della Comunicazione</strong> presso <strong>Politecnico di Milano</strong>");
+	$("[name='__edu-01-h6']").html("dal 2018 ad oggi");
+	$("[name='__edu-02-h3']").html("<strong>Laurea di Primo Livello</strong> in <strong>Design della Comunicazione</strong> presso <strong>Politecnico di Milano</strong>");
+	$("[name='__edu-02-h4']").html("<em>Pitybear. Ascolta, partecipa, immedesimati</em>");
+	$("[name='__edu-02-h6']").html("12 settembre 2018");
+	$("[name='__edu-03-h3']").html("<strong>Corso di formazione</strong> in <strong>Type Design</strong> presso <strong>CFP Bauer</strong> (Milano)");
+	$("[name='__edu-03-h6']").html("marzo&ndash;aprile 2015");
+	$("[name='__edu-04-h3']").html("<strong>Diploma</strong> presso <strong>Liceo Artistico U. Boccioni</strong> (Milano)");
+
+	$("[name='__skls-header'").html('<i class="fas fa-sm fa-lightbulb"></i> Conoscenze');
+	$("[name='__skls-01-td'").html('Italiano<span class="small"> &Colon; madrelingua</span>');
+	$("[name='__skls-02-td'").html('Inglese<span class="small"> &Colon; TOEIC C1</span>');
+
+	$("[name='__cont-header'").html('<i class="fas fa-sm fa-user"></i> Contatti');
+
+	$("[name='__ints-header'").html('<i class="fas fa-sm fa-star"></i> Interessi');
+
+	$("[name='__work-header'").html('<i class="fas fa-sm fa-briefcase"></i> Esperienze lavorative');
+};
+
+var en = function(){
+	$("[name='__edu-header'").html('<i class="fas fa-sm fa-graduation-cap"></i> Education');
+	$("[name='__edu-01-h3']").html("<strong>Master's Degree</strong> in <strong>Communication Design</strong> at <strong>Politecnico di Milano</strong>");
+	$("[name='__edu-01-h6']").html("since 2018 (present)");
+	$("[name='__edu-02-h3']").html("<strong>Bachelor's Degree</strong> in <strong>Communication Design</strong> at <strong>Politecnico di Milano</strong>");
+	$("[name='__edu-02-h4']").html("<em>Pitybear. Ascolta, partecipa, immedesimati</em><br>[<em>TL: Pitybear. Listen, participate, relate</em>]");
+	$("[name='__edu-02-h6']").html("September 12, 2018");
+	$("[name='__edu-03-h3']").html("<strong>Formation  course</strong> in <strong>Type Design</strong> at <strong>CFP Bauer</strong> (Milan)");
+	$("[name='__edu-03-h6']").html("March&ndash;April 2015");
+	$("[name='__edu-04-h3']").html("<strong>Diploma</strong> at <strong>Liceo Artistico U. Boccioni</strong> (Milan)");
+
+	$("[name='__skls-header'").html('<i class="fas fa-sm fa-lightbulb"></i> Skills');
+	$("[name='__skls-01-td'").html('Italian<span class="small"> &Colon; native speaker</span>');
+	$("[name='__skls-02-td'").html('English<span class="small"> &Colon; TOEIC C1</span>');
+
+	$("[name='__cont-header'").html('<i class="fas fa-sm fa-user"></i> Contact');
+
+	$("[name='__ints-header'").html('<i class="fas fa-sm fa-star"></i> Interests');
+
+	$("[name='__work-header'").html('<i class="fas fa-sm fa-briefcase"></i> Work experience');
+};
