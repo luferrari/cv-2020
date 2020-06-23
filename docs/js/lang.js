b735f15b3e075c54f5f33e0c5b046a230014349d
luferrari <lucialuferrari@gmail.com>
2020-06-23 22:26:45 +0200

    Sixth pass

--- docs/js/lang.js
+++ docs/js/lang.js
@@ -1,43 +1,61 @@
 var it = function(){
-	$("[name='__edu-header'").html('<i class="fas fa-sm fa-graduation-cap"></i> Istruzione');
-	$("[name='__edu-01-h3']").html("<strong>Laurea Magistrale</strong> in <strong>Design della Comunicazione</strong> presso <strong>Politecnico di Milano</strong>");
-	$("[name='__edu-01-h6']").html("dal 2018 ad oggi");
-	$("[name='__edu-02-h3']").html("<strong>Laurea di Primo Livello</strong> in <strong>Design della Comunicazione</strong> presso <strong>Politecnico di Milano</strong>");
-	$("[name='__edu-02-h4']").html("<em>Pitybear. Ascolta, partecipa, immedesimati</em>");
-	$("[name='__edu-02-h6']").html("12 settembre 2018");
-	$("[name='__edu-03-h3']").html("<strong>Corso di formazione</strong> in <strong>Type Design</strong> presso <strong>CFP Bauer</strong> (Milano)");
-	$("[name='__edu-03-h6']").html("marzo&ndash;aprile 2015");
-	$("[name='__edu-04-h3']").html("<strong>Diploma</strong> presso <strong>Liceo Artistico U. Boccioni</strong> (Milano)");
+	$('[name="__edu-header"').html('<i class="fas fa-sm fa-graduation-cap"></i> Istruzione');
+		$('[name="__edu-UB-h3"]').html('<strong>Diploma</strong> presso <strong>Liceo Artistico U. Boccioni</strong> (Milano)');
+		$('[name="__edu-UB-h4"]').html('<em>Arte astratta e teosofia</em>');
 
-	$("[name='__skls-header'").html('<i class="fas fa-sm fa-lightbulb"></i> Conoscenze');
-	$("[name='__skls-01-td'").html('Italiano<span class="small"> &Colon; madrelingua</span>');
-	$("[name='__skls-02-td'").html('Inglese<span class="small"> &Colon; TOEIC C1</span>');
+		$('[name="__edu-CF-small"]').html('marzo&ndash;aprile 2015');
+		$('[name="__edu-CF-h3"]').html('<strong>Corso di formazione</strong> in <strong>Type Design</strong> presso <strong>CFP Bauer</strong> (Milano)');
 
-	$("[name='__cont-header'").html('<i class="fas fa-sm fa-user"></i> Contatti');
+		$('[name="__edu-LT-small"]').html('12 settembre 2018');
+		$('[name="__edu-LT_CeP-h4"]').html('<i class="fas fa-info-circle pr-1" data-toggle="collapse" aria-expanded="false" href="#__edu-LT_CeP-coll" aria-controls="__edu-LT_CeP-coll"></i>[COLORE E PERCEZIONE] <strong>Color Emotion Mapping. Analisi e Progetto del Colore</strong><br>&Colon; Concept, design, presentazione');
+		$('[name="__edu-LT_CeP-info"]').html('Analisi della componente cromatica, gestione dati colore, comunicazione del colore, progetto del colore di una linea di prodotti a scelta, in questo caso le capsule di caffè Nespresso. Il risultato è un’ipotesi progettuale di una linea di caffè agli aromi di fiori e radici, contenuti in capsule dalle texture evocative delle note aromatiche.');
+		$('[name="__edu-LT_EL-h3"]').html('<strong>Laurea di Primo Livello</strong> in <strong>Design della Comunicazione</strong> presso <strong>Politecnico di Milano</strong>');
+		$('[name="__edu-LT_EL-h4"]').html('<i class="fas fa-info-circle pr-1" data-toggle="collapse" href="#__edu-LT_EL-coll" aria-expanded="false" aria-controls="__edu-LT_EL-coll"></i>[TESI] <em>Pitybear. Ascolta, partecipa, immedesimati</em><br>&Colon; Concept, design, progettazione, realizzazione');
+		$('[name="__edu-LT_EL-info"]').html('Valori, stati dell’essere, emozioni di tipo universale, questi i punti di partenza sui quali costruire un’esperienza comunicativa che interagisca con l’utente. L’obiettivo del corso è stato quello di progettare un’esperienza capace di trasmettere e comunicare questi complessi aspetti dell’emozione umana, attraverso la realizzazione di un dispositivo comunicativo che fosse in grado di instaurare una relazione con l’utente, innescando un processo di riflessione e coinvolgimento.');
 
-	$("[name='__ints-header'").html('<i class="fas fa-sm fa-star"></i> Interessi');
+		$('[name="__edu-LM-small"]').html('dal 2018 ad oggi');
+		$('[name="__edu-LM-h3"]').html('<strong>Laurea Magistrale</strong> in <strong>Design della Comunicazione</strong> presso <strong>Politecnico di Milano</strong>');
+		$('[name="__edu-LM-DfS-h4"]').html('<i class="fas fa-info-circle pr-1" data-toggle="collapse" aria-expanded="false" href="#__edu-LM-DfS-coll" aria-controls="__edu-LM-DfS-coll"></i>[DESIGN FOR SUSTAINABILITY] <strong>0KMeals</strong><br>&Colon; Concept, design, presentazione');
+		$('[name="__edu-LM-DfS-info"]').html('[trad.] Un imprenditore locale decide di aprire un food truck a energia solare. Fa un accordo con un’azienda che fornisce furgoni a energia solare. Il cibo viene dall’orto del Polimi dove gli studenti possono coltivare su base volontaria in cambio di buoni da usare nel food truck. Un’altra azienda fornirà gli strumenti per il giardinaggio e il know-how. Il sistema è sostenibile in quanto l’acqua è raccolta in serbatoi per il recupero dell’acqua piovana. In caso di poche piogge, il Comune di Milano può fornire l’acqua per l’irrigazione. I materiali di scarto del Polimi Lab vengono riciclati in piatti e posate con cui viene servito il cibo.');
 
-	$("[name='__work-header'").html('<i class="fas fa-sm fa-briefcase"></i> Esperienze lavorative');
+	$('[name="__skills-header"').html('<i class="fas fa-sm fa-lightbulb"></i> Conoscenze');
+	$('[name="__skills-01-td"').html('Italiano<span class="small"> &Colon; madrelingua</span>');
+	$('[name="__skills-02-td"').html('Inglese<span class="small"> &Colon; TOEIC C1</span>');
+
+	$('[name="__contact-header"').html('<i class="fas fa-sm fa-user"></i> Contatti');
+
+	$('[name="__interests-header"').html('<i class="fas fa-sm fa-star"></i> Interessi');
+
+	$('[name="__work-header"').html('<i class="fas fa-sm fa-briefcase"></i> Esperienze lavorative');
 };
 
 var en = function(){
-	$("[name='__edu-header'").html('<i class="fas fa-sm fa-graduation-cap"></i> Education');
-	$("[name='__edu-01-h3']").html("<strong>Master's Degree</strong> in <strong>Communication Design</strong> at <strong>Politecnico di Milano</strong>");
-	$("[name='__edu-01-h6']").html("since 2018 (present)");
-	$("[name='__edu-02-h3']").html("<strong>Bachelor's Degree</strong> in <strong>Communication Design</strong> at <strong>Politecnico di Milano</strong>");
-	$("[name='__edu-02-h4']").html("<em>Pitybear. Ascolta, partecipa, immedesimati</em><br>[<em>TL: Pitybear. Listen, participate, relate</em>]");
-	$("[name='__edu-02-h6']").html("September 12, 2018");
-	$("[name='__edu-03-h3']").html("<strong>Formation  course</strong> in <strong>Type Design</strong> at <strong>CFP Bauer</strong> (Milan)");
-	$("[name='__edu-03-h6']").html("March&ndash;April 2015");
-	$("[name='__edu-04-h3']").html("<strong>Diploma</strong> at <strong>Liceo Artistico U. Boccioni</strong> (Milan)");
+	$('[name="__edu-header"').html('<i class="fas fa-sm fa-graduation-cap"></i> Education');
+		$('[name="__edu-UB-h3"]').html('<strong>Diploma</strong> at <strong>Liceo Artistico U. Boccioni</strong> (Milan)');
+		$('[name="__edu-UB-h4"]').html('<em>Arte astratta e teosofia</em><br>[TL] <em>Abstract art and theosophy</em>');
 
-	$("[name='__skls-header'").html('<i class="fas fa-sm fa-lightbulb"></i> Skills');
-	$("[name='__skls-01-td'").html('Italian<span class="small"> &Colon; native speaker</span>');
-	$("[name='__skls-02-td'").html('English<span class="small"> &Colon; TOEIC C1</span>');
+		$('[name="__edu-CF-small"]').html('March&ndash;April 2015');
+		$('[name="__edu-CF-h3"]').html('<strong>Formation  course</strong> in <strong>Type Design</strong> at <strong>CFP Bauer</strong> (Milan)');
 
-	$("[name='__cont-header'").html('<i class="fas fa-sm fa-user"></i> Contact');
+		$('[name="__edu-LT-small"]').html('September 12, 2018');
+		$('[name="__edu-LT_CeP-h4"]').html('<i class="fas fa-info-circle pr-1" data-toggle="collapse" aria-expanded="false" href="#__edu-LT_CeP-coll" aria-controls="__edu-LT_CeP-coll"></i>[TL] [COLOR AND PERCEPTION] <strong>Color Emotion Mapping. Color Analysis e Design</strong><br>&Colon; Concept, design, presentation');
+		$('[name="__edu-LT_CeP-info"]').html('Color components analysis, color data management, color communication, color design of a chosen line of products, in this case Nespresso coffee capsules. The result is a design hypothesis of a line of flower- and root-scented coffees, contained in capsules with textures that evoke the aromas.');
+		$('[name="__edu-LT_EL-h3"]').html('<strong>Bachelor\'s Degree</strong> in <strong>Communication Design</strong> at <strong>Politecnico di Milano</strong>');
+		$('[name="__edu-LT_EL-h4"]').html('<i class="fas fa-info-circle pr-1" data-toggle="collapse" href="#__edu-LT_EL-coll" aria-expanded="false" aria-controls="__edu-LT_EL-coll"></i>[THESIS] <em>Pitybear. Ascolta, partecipa, immedesimati</em><br>[TL] Pitybear. Listen, participate, relate<br>&Colon; Concept, design, planning, execution');
+		$('[name="__edu-LT_EL-info"]').html('[TL] Values, states of being, universal emotions, these are the starting points from which to construct a communicative experience that interacts with the user. The objective of the course has been to design an experience capable of transmitting and communicating these complex aspects of human emotions, through the realization of a communicative device that could establish a relationship with the user, triggering a process of reflection and involvement.');
 
-	$("[name='__ints-header'").html('<i class="fas fa-sm fa-star"></i> Interests');
+		$('[name="__edu-LM-h3"]').html('<strong>Master\'s Degree</strong> in <strong>Communication Design</strong> at <strong>Politecnico di Milano</strong>');
+		$('[name="__edu-LM-small"]').html('since 2018 (present)');
+		$('[name="__edu-LM-DfS-h4"]').html('<i class="fas fa-info-circle pr-1" data-toggle="collapse" aria-expanded="false" href="#__edu-LM-DfS-coll" aria-controls="__edu-LM-DfS-coll"></i>[DESIGN FOR SUSTAINABILITY] <strong>0KMeals</strong><br>&Colon; Concept, design, presentation');
+		$('[name="__edu-LM-DfS-info"]').html('A local entrepreneur decides to open a solar-powered food truck. They reach an agreement with a company that provides solar-powered trucks. The food served comes from the Polimi garden where students can volunteer to work in exchange for coupons to be spent at the food truck. A company will provide the gardening tools along with the know-how.​ The system is sustainable in that the water is gathered from rain pools provided by a manufacturer. In case of low rain, the City of Milan can provide the irrigation. Leftover materials from Polimi Lab are recycled into dishes and cutlery with which the food is served.​');
 
-	$("[name='__work-header'").html('<i class="fas fa-sm fa-briefcase"></i> Work experience');
+	$('[name="__skills-header"').html('<i class="fas fa-sm fa-lightbulb"></i> Skills');
+		$('[name="__skills-01-td"').html('Italian<span class="small"> &Colon; native speaker</span>');
+		$('[name="__skills-02-td"').html('English<span class="small"> &Colon; TOEIC C1</span>');
+
+	$('[name="__contact-header"').html('<i class="fas fa-sm fa-user"></i> Contact');
+
+	$('[name="__interests-header"').html('<i class="fas fa-sm fa-star"></i> Interests');
+
+	$('[name="__work-header"').html('<i class="fas fa-sm fa-briefcase"></i> Work experience');
 };
