6e96087675d99e68af3060afcd70ea32ca43ba1b
luferrari <lucialuferrari@gmail.com>
2020-06-24 23:14:39 +0200

    Ninth pass

--- docs/js/lang.js
+++ docs/js/lang.js
@@ -1,33 +1,33 @@
 var it = function(){
 	$('[name="__edu-header"').html('<i class="fas fa-sm fa-graduation-cap"></i> Istruzione');
-		$('[name="__edu-UB-h3"]').html('<strong>Diploma</strong> presso <strong>Liceo Artistico U. Boccioni</strong> (Milano)');
-		$('[name="__edu-UB-h4"]').html('<em>Arte astratta e teosofia</em>');
-
-		$('[name="__edu-CF-small"]').html('marzo&ndash;aprile 2015');
-		$('[name="__edu-CF-h3"]').html('<strong>Corso di formazione</strong> in <strong>Type Design</strong> presso <strong>CFP Bauer</strong> (Milano)');
-
-		$('[name="__edu-LT-small"]').html('12 settembre 2018');
-		$('[name="__edu-LT_CeP-h4"]').html('[COLORE E PERCEZIONE] <strong>Color Emotion Mapping. Analisi e Progetto del Colore</strong><br>&Colon; Concept, design, presentazione<br><i class="fas fa-plus-circle" data-toggle="collapse" aria-expanded="false" href="#__edu-LT_CeP-coll" aria-controls="__edu-LT_CeP-coll"> <span>INFO</span></i>');
-		$('[name="__edu-LT_CeP-info"]').html('Analisi della componente cromatica, gestione dati colore, comunicazione del colore, progetto del colore di una linea di prodotti a scelta, in questo caso le capsule di caffè Nespresso. Il risultato è un’ipotesi progettuale di una linea di caffè agli aromi di fiori e radici, contenuti in capsule dalle texture evocative delle note aromatiche.');
-		$('[name="__edu-LT_EL-h3"]').html('<strong>Laurea di Primo Livello</strong> in <strong>Design della Comunicazione</strong> presso <strong>Politecnico di Milano</strong>');
-		$('[name="__edu-LT_EL-h4"]').html('[TESI] <em>Pitybear. Ascolta, partecipa, immedesimati</em><br>&Colon; Concept, design, progettazione, esposizione<div>101/110</div><i class="fas fa-plus-circle" data-toggle="collapse" href="#__edu-LT_EL-coll" aria-expanded="false" aria-controls="__edu-LT_EL-coll"> <span>INFO</span></i>');
-		$('[name="__edu-LT_EL-info"]').html('Valori, stati dell’essere, emozioni di tipo universale, questi i punti di partenza sui quali costruire un’esperienza comunicativa che interagisca con l’utente. L’obiettivo del corso è stato quello di progettare un’esperienza capace di trasmettere e comunicare questi complessi aspetti dell’emozione umana, attraverso la realizzazione di un dispositivo comunicativo che fosse in grado di instaurare una relazione con l’utente, innescando un processo di riflessione e coinvolgimento.');
-
 		$('[name="__edu-LM-small"]').html('dal 2018 ad oggi');
-		$('[name="__edu-LM-h3"]').html('<strong>Laurea Magistrale</strong> in <strong>Design della Comunicazione</strong> presso <strong>Politecnico di Milano</strong>');
-		$('[name="__edu-LM-DfS-h4"]').html('[DESIGN FOR SUSTAINABILITY] <strong>0KMeals</strong><br>&Colon; Concept, design, presentazione<br><i class="fas fa-plus-circle" data-toggle="collapse" aria-expanded="false" href="#__edu-LM-DfS-coll" aria-controls="__edu-LM-DfS-coll"> <span>INFO</span></i>');
+		$('[name="__edu-LM-h3"]').html('Corso di <strong>Laurea Magistrale</strong> in <strong>Design della Comunicazione</strong> presso <strong>Politecnico di Milano</strong>');
+		$('[name="__edu-LM-DfS-h4"]').html('<em>0KMeals. Food system sostenibile per il Campus Polimi Bovisa</em><br>&Colon; Concept, design, presentazione (lavoro in gruppo)<br><i class="fas fa-plus-circle" data-toggle="collapse" aria-expanded="false" href="#__edu-LM-DfS-coll" aria-controls="__edu-LM-DfS-coll"> <span>INFO</span></i>');
 		$('[name="__edu-LM-DfS-info"]').html('Un imprenditore locale decide di aprire un food truck a energia solare. Fa un accordo con un’azienda che fornisce furgoni a energia solare. Il cibo viene dall’orto del Polimi dove gli studenti possono coltivare su base volontaria in cambio di buoni da usare nel food truck. Un’altra azienda fornirà gli strumenti per il giardinaggio e il know-how. Il sistema è sostenibile in quanto l’acqua è raccolta in serbatoi per il recupero dell’acqua piovana. In caso di poche piogge, il Comune di Milano può fornire l’acqua per l’irrigazione. I materiali di scarto del Polimi Lab vengono riciclati in piatti e posate con cui viene servito il cibo.');
 
+		$('[name="__edu-LT-small"]').html('12 settembre 2018');
+		$('[name="__edu-LT_EL-h3"]').html('<strong>Laurea di Primo Livello</strong> in <strong>Design della Comunicazione</strong> presso <strong>Politecnico di Milano</strong>');
+		$('[name="__edu-LT_EL-h4"]').html('<em>Pitybear. Ascolta, partecipa, immedesimati</em><br>&Colon; Concept, design, progettazione, esposizione (lavoro in gruppo)<div>101/110</div><i class="fas fa-plus-circle" data-toggle="collapse" href="#__edu-LT_EL-coll" aria-expanded="false" aria-controls="__edu-LT_EL-coll"> <span>INFO</span></i>');
+		$('[name="__edu-LT_EL-info"]').html('Valori, stati dell’essere, emozioni di tipo universale, questi i punti di partenza sui quali costruire un’esperienza comunicativa che interagisca con l’utente. L’obiettivo del corso è stato quello di progettare un’esperienza capace di trasmettere e comunicare questi complessi aspetti dell’emozione umana, attraverso la realizzazione di un dispositivo comunicativo che fosse in grado di instaurare una relazione con l’utente, innescando un processo di riflessione e coinvolgimento.');
+		$('[name="__edu-LT_CeP-h4"]').html('<em>Color Emotion Mapping. Analisi e Progetto del Colore</em><br>&Colon; Concept, design, presentazione<br><i class="fas fa-plus-circle" data-toggle="collapse" aria-expanded="false" href="#__edu-LT_CeP-coll" aria-controls="__edu-LT_CeP-coll"> <span>INFO</span></i>');
+		$('[name="__edu-LT_CeP-info"]').html('Analisi della componente cromatica, gestione dati colore, comunicazione del colore, progetto del colore di una linea di prodotti a scelta, in questo caso le capsule di caffè Nespresso. Il risultato è un’ipotesi progettuale di una linea di caffè agli aromi di fiori e radici, contenuti in capsule dalle texture evocative delle note aromatiche.');
+
+		$('[name="__edu-CF-small"]').html('marzo&ndash;aprile 2015');
+		$('[name="__edu-CF-h3"]').html('<strong>Corso di formazione</strong> in <strong>Type Design</strong> presso <strong>CFP Bauer</strong>, Milano');
+
+		$('[name="__edu-UB-h3"]').html('<strong>Diploma</strong> presso <strong>Liceo Artistico U. Boccioni</strong>, Milano');
+		$('[name="__edu-UB-h4"]').html('<em>Arte astratta e teosofia</em><div>94/100</div>');
+
 	$('[name="__skills-header"').html('<i class="fas fa-sm fa-lightbulb"></i> Conoscenze');
-	$('[name="__skills-01-td"').html('Italiano<span class="small"> &Colon; madrelingua</span>');
-	$('[name="__skills-02-td"').html('Inglese<span class="small"> &Colon; TOEIC C1</span>');
+		$('[name="__skills-ita-caption"').html('Italiano<span class="small"> &Colon; madrelingua</span>');
+		$('[name="__skills-eng-caption"').html('Inglese<span class="small"> &Colon; TOEIC C1</span>');
 
 	$('[name="__about-header"').html('<i class="fas fa-sm fa-user"></i> Su di me');
 		$('[name="__about-01"').html('Lucia Ferrari, classe 1995, laureanda in Design della Comunicazione al Politecnico di Milano.');
 		$('[name="__about-02"').html('Una forte attitudine creativa, associata ad una grande passione per la musica, ha trovato il suo sbocco nella produzione digitale di brani di musica elettronica e sperimentale.');
-		$('[name="__about-03"').html('Da sempre ispirata dal mondo dei videogiochi e dell’animazione, tra cui di origine giapponese, spesso ne utilizzo il linguaggio come ispirazione per i miei lavori di graphic design.');
-		$('[name="__about-04"').html('Mi affascina molto lo studio e l’utilizzo del segno grafico. Per questo la tipografia è un’altra mia passione e mi interessa molto l’impatto che caratteri diversi hanno in un progetto di comunicazione.');
-		$('[name="__about-05"').html('Trovo stimolante infine lo studio e l’utilizzo delle lingue straniere, specialmente se utilizzano sistemi di scrittura non latini.');
+		$('[name="__about-03"').html('Da sempre ispirata dal mondo dei videogiochi e dell’animazione, spesso ne utilizzo il linguaggio come ispirazione per i miei lavori di graphic design.');
+		$('[name="__about-04"').html('Mi affascina molto lo studio e l’utilizzo del segno grafico. Per questo la tipografia è un’altra mia passione, con il relativo interesse sull’impatto con il relativo interesse sull’impatto di caratteri diversi in un progetto di comunicazione.');
+		$('[name="__about-05"').html('Trovo stimolante infine lo studio e l’utilizzo delle lingue straniere, specialmente se utilizzano sistemi di scrittura non alfabetici.');
 
 	$('[name="__work-header"').html('<i class="fas fa-sm fa-briefcase"></i> Esperienze lavorative');
 
@@ -36,27 +36,27 @@ var it = function(){
 
 var en = function(){
 	$('[name="__edu-header"').html('<i class="fas fa-sm fa-graduation-cap"></i> Education');
-		$('[name="__edu-UB-h3"]').html('<strong>Diploma</strong> at <strong>Liceo Artistico U. Boccioni</strong> (Milan)');
-		$('[name="__edu-UB-h4"]').html('<em>Arte astratta e teosofia</em><br><em>Abstract art and theosophy</em>');
-
-		$('[name="__edu-CF-small"]').html('March&ndash;April 2015');
-		$('[name="__edu-CF-h3"]').html('<strong>Formation  course</strong> in <strong>Type Design</strong> at <strong>CFP Bauer</strong> (Milan)');
-
-		$('[name="__edu-LT-small"]').html('September 12, 2018');
-		$('[name="__edu-LT_CeP-h4"]').html('[COLOR AND PERCEPTION] <strong>Color Emotion Mapping. Color Analysis e Design</strong><br>&Colon; Concept, design, presentation<br><i class="fas fa-plus-circle" data-toggle="collapse" aria-expanded="false" href="#__edu-LT_CeP-coll" aria-controls="__edu-LT_CeP-coll"> <span>INFO</span></i>');
-		$('[name="__edu-LT_CeP-info"]').html('Color components analysis, color data management, color communication, color design of a chosen line of products, in this case Nespresso coffee capsules. The result is a design hypothesis of a line of flower- and root-scented coffees, contained in capsules with textures that evoke the aromas.');
-		$('[name="__edu-LT_EL-h3"]').html('<strong>Bachelor’s Degree</strong> in <strong>Communication Design</strong> at <strong>Politecnico di Milano</strong>');
-		$('[name="__edu-LT_EL-h4"]').html('[THESIS] <em>Pitybear. Ascolta, partecipa, immedesimati</em><br>Pitybear. Listen, participate, relate<br>&Colon; Concept, design, planning, exposition<div>101/110</div><i class="fas fa-plus-circle" data-toggle="collapse" href="#__edu-LT_EL-coll" aria-expanded="false" aria-controls="__edu-LT_EL-coll"> <span>INFO</span></i>');
-		$('[name="__edu-LT_EL-info"]').html('Values, states of being, universal emotions, these are the starting points from which to construct a communicative experience that interacts with the user. The objective of the course has been to design an experience capable of transmitting and communicating these complex aspects of human emotions, through the realization of a communicative device that could establish a relationship with the user, triggering a process of reflection and involvement.');
-
-		$('[name="__edu-LM-h3"]').html('<strong>Master’s Degree</strong> in <strong>Communication Design</strong> at <strong>Politecnico di Milano</strong>');
+		$('[name="__edu-LM-h3"]').html('<strong>Master’s Degree Course</strong> in <strong>Communication Design</strong> at <strong>Politecnico di Milano</strong>');
 		$('[name="__edu-LM-small"]').html('since 2018 (present)');
-		$('[name="__edu-LM-DfS-h4"]').html('[DESIGN FOR SUSTAINABILITY] <strong>0KMeals</strong><br>&Colon; Concept, design, presentation<br><i class="fas fa-plus-circle" data-toggle="collapse" aria-expanded="false" href="#__edu-LM-DfS-coll" aria-controls="__edu-LM-DfS-coll"> <span>INFO</span></i>');
+		$('[name="__edu-LM-DfS-h4"]').html('<em>0KMeals. Sustainable food system for Polimi Bovisa Campus</em><br>&Colon; Concept, design, presentation (group work)<br><i class="fas fa-plus-circle" data-toggle="collapse" aria-expanded="false" href="#__edu-LM-DfS-coll" aria-controls="__edu-LM-DfS-coll"> <span>INFO</span></i>');
 		$('[name="__edu-LM-DfS-info"]').html('A local entrepreneur decides to open a solar-powered food truck. They reach an agreement with a company that provides solar-powered trucks. The food served comes from the Polimi garden where students can volunteer to work in exchange for coupons to be spent at the food truck. A company will provide the gardening tools along with the know-how.​ The system is sustainable in that the water is gathered from rain pools provided by a manufacturer. In case of low rain, the City of Milan can provide the irrigation. Leftover materials from Polimi Lab are recycled into dishes and cutlery with which the food is served.​');
 
+		$('[name="__edu-LT-small"]').html('September 12, 2018');
+		$('[name="__edu-LT_EL-h3"]').html('<strong>Bachelor’s Degree</strong> in <strong>Communication Design</strong> at <strong>Politecnico di Milano</strong>');
+		$('[name="__edu-LT_EL-h4"]').html('<em>Pitybear. Ascolta, partecipa, immedesimati</em><br>Pitybear. Listen, participate, relate<br>&Colon; Concept, design, planning, exposition (group work)<div>101/110</div><i class="fas fa-plus-circle" data-toggle="collapse" href="#__edu-LT_EL-coll" aria-expanded="false" aria-controls="__edu-LT_EL-coll"> <span>INFO</span></i>');
+		$('[name="__edu-LT_EL-info"]').html('Values, states of being, universal emotions, these are the starting points from which to construct a communicative experience that interacts with the user. The objective of the course has been to design an experience capable of transmitting and communicating these complex aspects of human emotions, through the realization of a communicative device that could establish a relationship with the user, triggering a process of reflection and involvement.');
+		$('[name="__edu-LT_CeP-h4"]').html('<em>Color Emotion Mapping. Color Analysis e Design</em><br>&Colon; Concept, design, presentation<br><i class="fas fa-plus-circle" data-toggle="collapse" aria-expanded="false" href="#__edu-LT_CeP-coll" aria-controls="__edu-LT_CeP-coll"> <span>INFO</span></i>');
+		$('[name="__edu-LT_CeP-info"]').html('Color components analysis, color data management, color communication, color design of a chosen line of products, in this case Nespresso coffee capsules. The result is a design hypothesis of a line of flower- and root-scented coffees, contained in capsules with textures that evoke the aromas.');
+
+		$('[name="__edu-CF-small"]').html('March&ndash;April 2015');
+		$('[name="__edu-CF-h3"]').html('<strong>Formation course</strong> in <strong>Type Design</strong> at <strong>CFP Bauer</strong>, Milan');
+
+		$('[name="__edu-UB-h3"]').html('<strong>Diploma</strong> at <strong>Liceo Artistico U. Boccioni</strong>, Milan');
+		$('[name="__edu-UB-h4"]').html('<em>Arte astratta e teosofia</em><br><em>Abstract art and theosophy</em><div>94/100</div>');
+
 	$('[name="__skills-header"').html('<i class="fas fa-sm fa-lightbulb"></i> Skills');
-		$('[name="__skills-01-td"').html('Italian<span class="small"> &Colon; native speaker</span>');
-		$('[name="__skills-02-td"').html('English<span class="small"> &Colon; TOEIC C1</span>');
+		$('[name="__skills-ita-caption"').html('Italian<span class="small"> &Colon; native speaker</span>');
+		$('[name="__skills-eng-caption"').html('English<span class="small"> &Colon; TOEIC C1</span>');
 
 	$('[name="__about-header"').html('<i class="fas fa-sm fa-user"></i> About');
 		$('[name="__about-01"').html('Lucia Ferrari, born 1995, graduate student in Communication Design at Politecnico di Milano.');
